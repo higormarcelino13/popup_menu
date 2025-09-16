c2d85b0b962549eb653c238d2b0c8c4856bc9260
Higor Marcelino <higor.freitas@aluno.faculdadeimpacta.com.br>
2025-09-16 13:32:32 -0300

    refactor: clean up test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,17 +5,12 @@ require "mongoid"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
     setup do
-      # Garante que o Mongoid está conectado ao ambiente de teste
       Mongoid.load!(File.expand_path("../config/mongoid.yml", __dir__), :test)
-      # Limpa todas as coleções entre os testes
       Mongoid.default_client.database.collections.each do |collection|
         next if collection.name.start_with?("system.")
         collection.delete_many
